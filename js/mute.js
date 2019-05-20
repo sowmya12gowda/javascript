593350fdd196b45703ff19371736f6ff972a06d6
sowmya <Sningegowdaostb1@gmail.com>
2019-05-20 16:50:24 +0530

    Js first commit

---
+++ js/mute.js
@@ -0,0 +1,11 @@
+var  firstname="helloworld<br>"; secondname="welcome<br>"; c="javascript<br>"; 
+ var s1="hai good afternoon";
+ var s2="had your lunch";
+
+document.write(firstname,secondname,c);
+document.write(s1+ " " +s2);
+
+
+
+
+
---
+++ js/task1.js
@@ -0,0 +1,4 @@
+var s1="welcome to  'javascript'";
+document.write(s1);
+
+
